55b3e6f50195672f6d0252cffbb7469b76c72284
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-09-20 23:19:16 +0530

    3G_pattern_with_cone_numeric.c

--- C Basics/3G_pattern_with_cone_numeric.c
+++ C Basics/3G_pattern_with_cone_numeric.c
@@ -8,7 +8,7 @@
 #include<stdio.h>
 void main(){
     int i, j, n;
-    printf("Enter the number: \n");
+    printf("Enter the number: \n")
     scanf("%d", &n);
     for(i=1; i<=n; i++){
         for(j=1; j<=n-i+1; j++){
