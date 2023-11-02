cd3ad7f8728d6840ca0d9df8dc8d41accecbc74f
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-11-02 22:05:40 +0530

    Update 5B_capitallize_kth_character.c

--- C Basics/5B_capitallize_kth_character.c
+++ C Basics/5B_capitallize_kth_character.c
@@ -5,8 +5,7 @@
 void main(){
     char str[20];
     int i, pos, len;
-    printf("Enter a string: \n")
-        
+    printf("Enter a string: \n");
     gets(str);
     printf("Enter a position: \n");
     scanf("%d", &pos);
