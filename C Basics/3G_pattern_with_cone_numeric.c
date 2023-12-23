68591565a9bdd41c71adc6ff3227520f62dfe0b1
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-12-23 18:04:49 +0530

    Update 3G_pattern_with_cone_numeric.c

--- C Basics/3G_pattern_with_cone_numeric.c
+++ C Basics/3G_pattern_with_cone_numeric.c
@@ -12,7 +12,7 @@ void main(){
     scanf("%d", &n);
     for(i=1; i<=n; i++){
         for(j=1; j<=n-i+1; j++){
-            printf("%d\t", j);
+            printf("%d\t", j)
         }
         printf("\n");
     }
