d0f8e4b718f09119cb6a9f00311c4aa45e16a327
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2024-01-17 21:32:53 +0530

    Update 3C_pattern_with_numeric_2.c

--- C Basics/3C_pattern_with_numeric_2.c
+++ C Basics/3C_pattern_with_numeric_2.c
@@ -9,7 +9,7 @@ void main(){
     int i, j, r, c;
     printf("Enter the number of rows: \n");
     scanf("%d", &r);
-    printf("Enter the number of column: \n")
+    printf("Enter the number of column: \n");
     scanf("%d", &c);
     for(i=1; i<=r; i++){
         for(j=1; j<=c; j++){
