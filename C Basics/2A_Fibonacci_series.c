b52608e58bd786602a684bfac642dc07a425ece1
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2024-02-03 23:37:40 +0530

    Update 2A_Fibonacci_series.c

--- C Basics/2A_Fibonacci_series.c
+++ C Basics/2A_Fibonacci_series.c
@@ -3,7 +3,7 @@ void main(){
     int num, n1, n2, n3, i;
     n1=0;
     n2=1;
-    printf("Enter the number of element: \n")
+    printf("Enter the number of element: \n");
     scanf("%d", &num);
     printf("%d\t%d\t", n1, n2);
     for(i=2; i<num; i++){
